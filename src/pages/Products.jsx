9741f61e480652bb649af9c1c8d9fdf1ee445546
Matias <matubil@gmail.com>
2023-11-09 15:59:04 -0300

    Update/ clase hasta 2:39:00

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,38 +1,56 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "boostrap/dist/css/boostrap.css";
+import "bootstrap/dist/css/bootstrap.css"
+import { useNavigate } from "react-router-dom";
 
-const Products = ({ titulo }) => {
+const Products = ({ titulo, ...restOfProps }) => { //con restOfProps llegan el resto de las cosas del objeto
 
-    let [productos, setProductos] = useState();
+    let [productos, setProductos] = useState([]); //Inicializamos con un array vacio
+    let navigate = useNavigate();
 
-    useEffect(()=> {
-        axios.get("http://localhost:3001/productos").then((resp)=>{
+    useEffect(() => {
+        axios.get("http://localhost:3001/productos").then((resp) => {
             setProductos(resp.data);
         })
     }, [])
 
-    return (() => {
+    return (
         <main>
             <h1>{titulo}</h1>
-            <table>
+
+            <button className="btn btn-success text-center w-100 mb-3" onClick={()=>{navigate("/")}}> {/* Lo que hace el navigate o useNavigate, es que puedas elegir a donde o que parte d etu pagina ir usando las routes que definimos */}
+                Volver
+            </button>
+
+            <table className="table">
                 <thead>
                     <tr>
                         <th>Nombre</th>
                         <th>Descripcion</th>
                         <th>Precio</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        productos.map((producto=>(
-                            
+                        productos.map((producto) => (
+                            <tr key={producto.nombre}>
+                                <td>{producto.nombre}</td>
+                                <td>{producto.descripcion}</td>
+                                <td>{producto.precio}</td>
+                                <td>
+                                    <button className="btn btn-primary float-end" onClick={()=>
+                                        {navigate(`/Products/${producto.id}`)}}>
+                                        Editar
+                                    </button>
+                                </td>
+                            </tr>
                         ))
                     }
                 </tbody>
             </table>
         </main>
-    })
-} 
+    )
+}
 
 export default Products;
